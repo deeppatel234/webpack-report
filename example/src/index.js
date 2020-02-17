23e65fd47c0b1f497d141881c4c18cb01098345f
Deep Patel <deeppatel234@gmail.com>
2020-02-17 19:43:04 +0530

    add example

---
+++ example/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+const App = () => {
+    return <div>Hello world</div>
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
