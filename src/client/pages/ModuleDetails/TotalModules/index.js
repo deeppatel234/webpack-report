57f4413cb4d1ea3d1d4efe840b3d90dccdad51a4
Deep Patel <deeppatel234@gmail.com>
2020-04-08 21:45:34 +0530

    add filter in modules

--- src/client/pages/ModuleDetails/TotalModules/index.js
+++ src/client/pages/ModuleDetails/TotalModules/index.js
@@ -1,9 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import AppContext from 'src/AppContext';
 import NofileIcon from 'Components/Icons/NoFile';
 import Table from 'Components/Table';
 import Empty from 'Components/Empty';
+
+import JavascriptIcon from 'Components/Icons/Javascript';
+import CSSIcon from 'Components/Icons/CSS';
+import FileIcon from 'Components/Icons/File';
+
+import Typography from 'Components/Typography';
+
+import { Wrapper, ListWrapper, ListItem, InfoWrapper } from './styled';
 
 const headers = [
   { key: 'name', header: 'Name', sort: true },
@@ -11,7 +19,25 @@ const headers = [
   { key: 'size', header: 'Size', fileSize: true, sort: true },
 ];
 
+const BadgeItem = ({ displayName, value, icon: Icon, ...props }) => (
+  <ListItem color="info" {...props}>
+    <Icon width="1.5rem" />
+    <InfoWrapper>
+      <Typography color="muted" variant="helpText" uppercase>
+        {displayName}
+      </Typography>
+      <Typography weight="600">{value}</Typography>
+    </InfoWrapper>
+  </ListItem>
+);
+
+const title = {
+  '.js': 'Javascript',
+  '.css': 'CSS',
+};
+
 const ModuleDetails = () => {
+  const [activeFilter, setFilter] = useState('');
   const { stateData } = useContext(AppContext);
   const { modules } = stateData;
 
@@ -23,13 +49,47 @@ const ModuleDetails = () => {
     );
   }
 
+  const jsModulesLength = modules.filter(m => m.name.endsWith('.js')).length;
+  const cssModulesLength = modules.filter(m => m.name.endsWith('.css')).length;
+
+  const filteredData = activeFilter
+    ? modules.filter(m => m.name.endsWith(activeFilter))
+    : modules;
+
   return (
-    <Table
-      searchKey="name"
-      title={`All Modules (${modules.length})`}
-      headers={headers}
-      data={modules}
-    />
+    <Wrapper>
+      <ListWrapper>
+        <BadgeItem
+          active={!activeFilter}
+          displayName="All"
+          value={modules.length}
+          icon={FileIcon}
+          onClick={() => setFilter(false)}
+        />
+        <BadgeItem
+          active={activeFilter === '.js'}
+          displayName="Javascript"
+          value={jsModulesLength}
+          icon={JavascriptIcon}
+          className="javascript"
+          onClick={() => setFilter('.js')}
+        />
+        <BadgeItem
+          active={activeFilter === '.css'}
+          displayName="CSS"
+          value={cssModulesLength}
+          icon={CSSIcon}
+          className="css"
+          onClick={() => setFilter('.css')}
+        />
+      </ListWrapper>
+      <Table
+        searchKey="name"
+        title={`${title[activeFilter] || 'All'} Modules`}
+        headers={headers}
+        data={filteredData}
+      />
+    </Wrapper>
   );
 };
 
